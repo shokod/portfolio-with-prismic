e9d49c50ddbb289d14f272ba61c4eb7b0cee1d42
ds-zion <shokodelvin@gmail.com>
2024-02-28 07:31:28 +0200

    Implemented text animation uisng gsap

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
@@ -15,8 +16,41 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   const component = useRef(null);
 
   useEffect(() => {
-    let ctx = gsap.context(() => {}, component);
-    return () => ctx.revert();
+    let ctx = gsap.context(() => {
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
+      gsap
+        .timeline()
+        .fromTo(
+          ".name-animation",
+          { x: -100, opacity: 0, rotate: -10 },
+          {
+            x: 0,
+            opacity: 1,
+            rotate: 0,
+
+            ease: "elastic.out(1,0.3)",
+            duration: 1,
+            transformOrigin: "left top",
+            stagger: { each: 0.1, from: "random" },
+          },
+        )
+        .fromTo(
+          ".job-title",
+          {
+            y: 20,
+            opacity: 0,
+            scale: 1.2,
+          },
+          {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            scale: 1,
+            ease: "elastic.out(1,0.3)",
+          },
+        );
+    }, component);
+    return () => ctx.revert(); // cleanup!
   }, []);
 
 
@@ -36,6 +70,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center" >
         <div className="col-start-1 md:row-start-1">
