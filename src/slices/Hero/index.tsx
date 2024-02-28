4c4070eb0ef650e97df9618a74c97294bdf0affa
ds-zion <shokodelvin@gmail.com>
2024-02-28 05:13:24 +0200

    Improved UX with responsive design

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,32 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center" >
+        <div className="col-start-1 md:row-start-1">
+          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter">
+            <span className="text-green-500">{slice.primary.first_name}</span>
+            <>{slice.primary.last_name}</>
+            <>{slice.primary.tagline}</>
+          </h1>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
