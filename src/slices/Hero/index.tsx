581ada7ee9cf2b92065dcecd339d84a88f43e0e1
ds-zion <shokodelvin@gmail.com>
2024-02-28 06:00:06 +0200

    Set up the gsap context for text animations

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client";
+import React, { useEffect, useRef } from "react";
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -10,6 +12,26 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {}, component);
+    return () => ctx.revert();
+  }, []);
+
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key}-index inline-block opacity-0 `}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -18,9 +40,9 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center" >
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + "" + slice.primary.last_name}>
-            <span className="block text-slate-300">{slice.primary.first_name}</span>
-            <span className="-mt-[.2em] block text-slate-500 ">{slice.primary.last_name}</span>
-            
+            <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
+            <span className="-mt-[.2em] block text-slate-500 ">{renderLetters(slice.primary.last_name, "last")}</span>
+
           </h1>
           <span className="job-title block bg-gradient-to-tr from-green-500 via-green-700 to-green-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tagline}</span>
 
