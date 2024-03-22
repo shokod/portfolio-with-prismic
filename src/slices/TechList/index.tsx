87b12c4f43fb438567ced600a92108e08feb914b
shokod <delvinshoko@gmail.com>
2024-03-22 12:09:49 +0200

    Add tech stack list as indexed rows

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -2,6 +2,7 @@ import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import React from "react";
+import { MdCircle } from "react-icons/md";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 /**
@@ -22,17 +23,20 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
         {slice.primary.heading}
       </Heading>
       {slice.items.map(({ tech_color, tech_name }, index) => (
-        <div key={index} >
+        <div key={index} className="tech-row mb-8 items-center justify-center gap-4 text-slate-700">
           {Array.from({ length: 15 }, (_, index) => (
             <React.Fragment key={index}>
               <span
-                className="tech-item text-8xl font-extrabold uppercase tracking-ti"
+                className="tech-item text-5xl font-extrabold uppercase tracking-ti"
                 style={{
-                  color: index === 7 && tech_color ? tech_color : "inherit"
+                  color: index === 3 && tech_color ? tech_color : "inherit"
                 }}
               >
                 {tech_name}
               </span>
+              {/* <span className="text-xl">
+                <MdCircle />
+              </span> */}
             </React.Fragment>
           ))}
           </div>
