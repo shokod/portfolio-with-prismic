cab947544f07c4614eedc1d185f0184d15427e3d
shokod <delvinshoko@gmail.com>
2024-03-23 09:35:46 +0200

    Created ContentList component for displaying list of content items with their tags

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,7 +1,35 @@
+import { Content } from '@prismicio/client';
 import React from 'react'
 
-export default function ContentList() {
-  return (
-    <div>ContentList</div>
-  )
+type ContentListProps = {
+    items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+    contentType: Content.ContentIndexSlice['primary']['content_type'];
+    fallbackItemImage: Content.ContentIndexSlice['primary']['fallback_item_image'];
+    viewMoreText: Content.ContentIndexSlice['primary']['view_more_text'];
+}
+
+export default function ContentList({ items, contentType, fallbackItemImage, viewMoreText = "View More" }: ContentListProps) {
+    return (
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                <li key={index}>
+                    <a href="">
+                        <div>
+                            <span>{item.data.title}</span>
+                            <div>
+                                {item.tags.map((tag, index) => (
+                                    <span key={index}>{tag}</span>
+
+                                ))}
+                            </div>
+                        </div>
+                        <span>{viewMoreText}</span>
+                    </a>
+                </li>
+                ))}
+            </ul>
+            
+        </div>
+    )
 }
--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -3,6 +3,7 @@ import Heading from "@/components/Heading";
 import { Content, isFilled } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import ContentList from "./ContentList";
+import { createClient } from "@/prismicio";
 
 
 /**
@@ -13,24 +14,36 @@ export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
 /**
  * Component for "ContentIndex" Slices.
  */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+const ContentIndex = async ({ slice }: ContentIndexProps): Promise<JSX.Element> => {
+  const client = createClient()
+  const blogPosts = await client.getAllByType('blog_post',)
+  const projects = await client.getAllByType('project',)
+
+  const contentType = slice.primary.content_type || "Blog"
+
+  const items = contentType === "Blog" ? blogPosts : projects
+
   return (
     <Bounded
-    data-slice-type={slice.slice_type}
-    data-slice-variation={slice.variation}
-  >
-    <Heading size='lg' className="mb-8">
-      {slice.primary.heading}
-    </Heading>
-    { isFilled.richText(slice.primary.description) && (
-      <div className="prose prose-xl prose-invert mb-10">
-        <PrismicRichText field={slice.primary.description} />
-      </div>
-    )}
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Heading size='lg' className="mb-8">
+        {slice.primary.heading}
+      </Heading>
+      {isFilled.richText(slice.primary.description) && (
+        <div className="prose prose-xl prose-invert mb-10">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
+      )}
 
-   <ContentList/>
-    
-  </Bounded>
+      <ContentList
+        items={items}
+        contentType={contentType} 
+        viewMoreText={slice.primary.view_more_text} 
+        fallbackItemImage={slice.primary.fallback_item_image} />
+
+    </Bounded>
   );
 };
 
