a0e2d08626f7f25d282bb87772e727b08758615f
shokod <delvinshoko@gmail.com>
2024-03-23 09:41:27 +0200

    Added some styling to the contentList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -11,20 +11,22 @@ type ContentListProps = {
 export default function ContentList({ items, contentType, fallbackItemImage, viewMoreText = "View More" }: ContentListProps) {
     return (
         <div>
-            <ul>
+            <ul className="grid border-b border-b-slate-100">
                 {items.map((item, index) => (
-                <li key={index}>
-                    <a href="">
-                        <div>
-                            <span>{item.data.title}</span>
-                            <div>
+                <li key={index}
+                className="list-item opacity-0">
+                    <a href=""
+                    className="flex flex-col justify-between border-t border-t-slate-100 py-10  text-slate-200 md:flex-row ">
+                        <div className="flex flex-col">
+                            <span className="text-3xl font-bold">{item.data.title}</span>
+                            <div className="flex gap-3 text-yellow-400">
                                 {item.tags.map((tag, index) => (
-                                    <span key={index}>{tag}</span>
+                                    <span key={index} className="text-lg font-bold">{tag}</span>
 
                                 ))}
                             </div>
                         </div>
-                        <span>{viewMoreText}</span>
+                        <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">{viewMoreText}</span>
                     </a>
                 </li>
                 ))}
