168812e058d55af90ab0b3bfb6ac812a7697b32a
shokod <delvinshoko@gmail.com>
2024-03-24 08:55:56 +0200

    Implemented dynamic image display and routing in ContentList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,6 +1,7 @@
-import { Content } from '@prismicio/client';
+"use client"
+import { Content, asImageSrc, isFilled } from '@prismicio/client';
 import Link from 'next/link';
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { MdArrowOutward } from 'react-icons/md';
 
 type ContentListProps = {
@@ -10,39 +11,67 @@ type ContentListProps = {
     viewMoreText: Content.ContentIndexSlice['primary']['view_more_text'];
 }
 
-export default function ContentList({ 
-    items, 
-    contentType, 
-    fallbackItemImage, 
+export default function ContentList({
+    items,
+    contentType,
+    fallbackItemImage,
     viewMoreText = "View More" }: ContentListProps) {
 
-        const urlprefix = contentType === "Blog" ? "/blog" : "/projects"
+    const urlprefix = contentType === "Blog" ? "/blog" : "/projects"
+    const component = useRef(null)
+    const [currentItem, setCurrentItem] = useState<null | number>(null)
+
+    const contentImages = items.map((item) => {
+        const image = isFilled.image(item.data.image) ? item.data.image : fallbackItemImage;
+
+        return asImageSrc(image, {
+            width: 220,
+            height: 320,
+            fit: 'crop',
+            exp: -10,
+        });
+    });
+
+
     return (
-        <div>
+        <div ref={component}>
             <ul className='grid border-b border-b-slate-100' >
                 {items.map((item, index) => (
-                    <li key={index}
-                    className='list-item opacity-0f'
-                       >
-                        <Link href={urlprefix + '/' + item.uid}
-                        className='flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row'
+                    <>
+                        {isFilled.keyText(item.data.title) && (
+                            <li key={index}
+                                className='list-item opacity-0f'
                             >
-                            <div className='flex flex-col' >
-                                <span className='text-2xl font-bold'>{item.data.title}
-                                </span>
-                                <div className='flex gap-3 text-green-400 text-lg font-mono'>
-                                    {item.tags.map((tag, index) => (
-                                        <span key={index}>{tag}</span>
+                                <Link href={urlprefix + '/' + item.uid}
+                                    className='flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row'
+                                    aria-label={item.data.title}
+                                >
+                                    <div className='flex flex-col' >
+                                        <span className='text-2xl font-bold'>{item.data.title}
+                                        </span>
+                                        <div className='flex gap-3 text-green-400 text-lg font-mono'>
+                                            {item.tags.map((tag, index) => (
+                                                <span key={index}>{tag}</span>
 
-                                    ))}
-                                </div>
-                            </div>
-                            <span className='ml-auto flex items-center gap-2 text-lg font-medium md:ml-0'>{viewMoreText}<MdArrowOutward/></span>
-                        </Link>
-                    </li>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <span className='ml-auto flex items-center gap-2 text-lg font-medium md:ml-0'>{viewMoreText}<MdArrowOutward /></span>
+                                </Link>
+                            </li>
+                        )}
+                    </>
                 ))}
             </ul>
+            {/* hover element */}
+            <div className=" hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-{background] duration-300"
+                style={{
+                    backgroundImage: currentItem !== null ? `url(${contentImages[currentItem]})` : '', 
+            }}
+            >
 
         </div>
+
+        </div >
     )
 }
