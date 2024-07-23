1e0b25b2f0ec56c33d6bb05f3cb907f47fc2e1f3
shokod <delvinshoko@gmail.com>
2024-07-23 15:00:47 +0200

    resume edit

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -35,7 +35,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
         <Avatar
         image={slice.primary.avatar}
         className="row-start-1 max-wsm md:col-start-2 md:row-end-3 border-r-green-600"
-        alt='Image of the author of the biography.'
+        alt='Image of the author.'
         />
       </div>
     </Bounded>
