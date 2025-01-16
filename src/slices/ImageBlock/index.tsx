e7ba3b5b70c2b278622468790c4bd1f4f49956e3
shokod <delvinshoko@gmail.com>
2025-01-16 22:34:31 +0200

    Fix: Image resizing

--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -13,7 +13,15 @@ export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
 const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
   return (
     
-      <PrismicNextImage  field={slice.primary.image} imgixParams={{ w: 600 }}></PrismicNextImage>
+    <div className="flex justify-center items-center p-4 sm:p-8">
+    <div className="max-w-2xl w-full overflow-hidden rounded-xl shadow-lg transition-all duration-300 hover:shadow-xl hover:-translate-y-1">
+      <PrismicNextImage
+        field={slice.primary.image}
+        imgixParams={{ w: 600 }}
+        className="w-full h-auto object-cover"
+      />
+    </div>
+  </div>
   );
 };
 
